7fff677f2539343f5577011d11d7c6dade67680e
RayChao2016 <raychao2016@gmail.com>
2019-07-21 23:03:56 +0800

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,10 +1,26 @@
-
 import requests
 from bs4 import BeautifulSoup
 import json
 import time
 import datetime
 
+
+def parseImage_v(request):
+    doc = requests.get(request)
+    soup = BeautifulSoup(doc.text, 'html.parser')
+    img1 = soup.find("meta", property="og:image")
+    type_p = soup.find("meta", property="og:type")
+
+    if img1 and type_p:
+        if type_p['content'] == "instapp:photo":
+            v_link = True
+        else:
+            v_link = False
+    else:
+        v_link = False
+
+    return v_link
+    
 
 def parseImage_2(request):
     doc = requests.get(request)
@@ -18,8 +34,9 @@ def parseImage_2(request):
     photo_id = js['shortcode']
     img = js['display_url']
     account = js['owner']['username']
+    account_id = js['owner']['id']
     date1 =  datetime.datetime.fromtimestamp(js['taken_at_timestamp'])
 
-    return account, img, date1, photo_id
+    return account, img, date1, photo_id, account_id
     
  
